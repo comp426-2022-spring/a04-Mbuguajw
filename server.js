a9bf3a14a16d92554ea21b4cf42233ecb6ade2ff
Janet Mbugua <mbuguaj@live.unc.edu>
2022-04-30 16:56:18 -0400

    why this error here??

--- server.js
+++ server.js
@@ -14,20 +14,18 @@ app.use(express.urlencoded({extended: true}));
 var HTTP_PORT = args.port || process.env.port || 5555;
 
 const help = (`
-server.js [options] 
+--port, -p	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535. Defaults to 5000.
 
---port	  Set the port number for the server to listen on. Must be an integer
-          between 1 and 65535.
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
 
---debug	  If set to true, creates endlpoints /app/log/access/ which returns
-          a JSON access log from the database and /app/error which throws 
-          an error with the message "Error test successful." Defaults to 
-          false.
+--loG   If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
 
---log		  If set to false, no log files are written. Defaults to true.
-          Logs are always written to database.
-
---help	  Return this message and exit.
+--help 	Return this message and exit.
 `)
 
 if (args.help || args.h) {
@@ -36,7 +34,7 @@ if (args.help || args.h) {
 }
 
 // Start server
-const server = app.listen(HTTP_PORT, () => {
+const accesslog = app.listen(HTTP_PORT, () => {
   console.log("App listening on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // Use morgan for logging to files
