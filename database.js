7a56a998004ab2f0236fd79ea66cc7a9829d8f1b
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-05-01 18:28:19 -0400

    SOMEWHERE I BELONGGGGGGGGGGGGGGGGGGGGG

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 // Connect to a database or create one if it doesn't exist yet.
-"use strict";
 const Database = require('better-sqlite3');
 const db = new Database('log.db');
 // Is the database initialized or do we need to initialize it?
 
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
-
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
+);
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
 // Check if there is a table. If row is undefined then no table exists.
